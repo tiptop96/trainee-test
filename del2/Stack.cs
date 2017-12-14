e904fd0a140ccbad8d5f9ce5ded345a5ae2909e4
Ove Lindström <ovelindstrom@users.noreply.github.com>
2017-12-14 13:29:35 +0100

    Added a C# interface also.

---
+++ del2/Stack.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OneTrack.Part2;
+{
+    interface Stack<T>
+    {
+
+        Stack<T> Push(T newItem);
+
+        T Pop();
+
+        Boolean Contains(T item);
+
+        T Access(T item);
+
+        Boolean IsEmpty();
+
+    }
+}
